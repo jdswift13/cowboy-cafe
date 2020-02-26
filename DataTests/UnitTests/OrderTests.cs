0750046467845892f39fe0e0b70fe006ec557343
Jacob Swift <jdswift13@ksu.edu>
2020-02-26 16:45:31 -0600

    tests done

--- DataTests/UnitTests/OrderTests.cs
+++ DataTests/UnitTests/OrderTests.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 using CowboyCafe.Data;
+using System.ComponentModel;
 namespace CowboyCafe.DataTests.UnitTests
 {
     public class OrderTests
     {
-        
+
         public class MockOrderItem : IOrderItem
         {
             public double Price { get; set; }
@@ -60,25 +61,51 @@ namespace CowboyCafe.DataTests.UnitTests
         [InlineData(new double[] { 1, 2, 3 })]
         [InlineData(new double[] { 0, 0, 0 })]
         [InlineData(new double[] { 199, 799 })]
-        [InlineData(new double[] { 798})]
+        [InlineData(new double[] { 798 })]
         [InlineData(new double[] { })]
-        [InlineData(new double[] { -5})]
-        [InlineData(new double[] { -4,10,8})]
-        [InlineData(new double[] { 3.13457890})]
-        [InlineData(new double[] { double.NaN})]
+        [InlineData(new double[] { -5 })]
+        [InlineData(new double[] { -4, 10, 8 })]
+        [InlineData(new double[] { 3.13457890 })]
+        [InlineData(new double[] { double.NaN })]
 
         public void SubtotalShouldBeSumOfAllItemPrices(double[] prices)
         {
             var order = new Order();
             double total = 0;
-            foreach(var price in prices)
+            foreach (var price in prices)
             {
                 total += price;
-                order.Add(new MockOrderItem(){
+                order.Add(new MockOrderItem() {
                     Price = price
                 });
             }
             Assert.Equal(total, order.Subtotal);
         }
+        [Fact]
+        [InlineData("Price")]
+        [InlineData("Items")]
+        public void AddingAnItemShouldTriggerPropertyChangedForPrice(string propertyName)
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            Assert.PropertyChanged(order, propertyName, () =>
+            {
+                order.Add(item);
+            });
+        }
+        [Theory]
+        [InlineData("Price")]
+        [InlineData("Items")]
+        public void RemovingAnItemShouldTriggerPropertyChanged(string propertyName) {
+            {
+                var order = new Order();
+                var item = new MockOrderItem();
+                Assert.PropertyChanged(order, propertyName, () =>
+                {
+                    order.Remove(item);
+                });
+
+            } }
+
     }
 }
