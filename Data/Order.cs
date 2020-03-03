ba76b459a5f30a8b08ea55fad7f881aa5b4dfa9e
Jacob Swift <jdswift13@ksu.edu>
2020-03-02 23:25:18 -0600

    help

--- Data/Order.cs
+++ Data/Order.cs
@@ -1,20 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 
 namespace CowboyCafe.Data
 {
     public class Order
     {
-        public double Subtotal => 0;
+        private List<IOrderItem> items = new List<IOrderItem>();
+        private double subtotal = 0;
+        private static uint lastOrderNumber;
+        public uint OrderNumber
+        {
+            get
+            {
+                lastOrderNumber++;
+                return lastOrderNumber + 1;
+
+            }
+        }
+        public double Subtotal
+        {
+            get
+            {
+                return subtotal;
+            }
+        }
         public IEnumerable<IOrderItem> Items => throw new NotImplementedException();
         public void Add(IOrderItem item)
         {
-
+            items.Add(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+            subtotal += item.Price;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
         }
         public void Remove(IOrderItem item)
         {
-
+            items.Remove(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+            subtotal -= item.Price;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
         }
+        public event PropertyChangedEventHandler PropertyChanged;
+        
     }
 }
